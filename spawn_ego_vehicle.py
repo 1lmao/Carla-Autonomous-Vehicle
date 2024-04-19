ca9efe2fde9215c944e8d275fd2b10289a4a691d
1lmao <smira009@ucr.edu>
2024-04-19 15:40:19 -0700

    adding spawn_ego_vehicle.py

--- spawn_ego_vehicle.py
+++ spawn_ego_vehicle.py
@@ -18,19 +18,3 @@ if 0 < number_of_spawn_points:
     print('\nEgo is spawned')
 else:
     logging.warning('Could not found any spawn points')
-
-
-#-------------------------
-#Spawn RGB camera and detector
-#-------------------------
-""" cam_bp = None
-cam_bp = world.get_blueprint_library().find('sensor.camera.rgb')
-cam_bp.set_attribute("image_size_x",str(1920))
-cam_bp.set_attribute("image_size_y",str(1080))
-cam_bp.set_attribute("fov",str(105))
-cam_location = carla.Location(2,0,1)
-cam_rotation = carla.Rotation(0,180,0)
-cam_transform = carla.Transform(cam_location,cam_rotation)
-ego_cam = world.spawn_actor(cam_bp,cam_transform,attach_to=ego_vehicle, attachment_type=carla.AttachmentType.Rigid)
-ego_cam.listen(lambda image: image.save_to_disk('tutorial/output/%.6d.jpg' % image.frame))
- """
