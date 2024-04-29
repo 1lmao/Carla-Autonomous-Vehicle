eec5d5c4b5b493debc2459f06557dc98dfeca6fd
1lmao <smira009@ucr.edu>
2024-04-28 19:19:18 -0700

    changed client.set_timeout(2000.0)

--- spawn_ego_vehicle.py
+++ spawn_ego_vehicle.py
@@ -4,16 +4,16 @@ import logging
 
 # Connect to the Carla server
 client = carla.Client('localhost', 2000)
-client.set_timeout(10.0)
+client.set_timeout(2000.0)
 
 # Retrieve the Carla world object
 world = client.get_world()
 
 # Retrieve the blueprint library
-blueprint_library = world.get_blueprint_library()
+#blueprint_library = world.get_blueprint_library()
 
 # Find the Tesla Model 3 blueprint
-ego_bp = blueprint_library.find('vehicle.tesla.model3')
+#ego_bp = blueprint_library.find('vehicle.tesla.model3')
 
 # Settings MUST BE SET YOU CAN RUN WORLD
 settings = world.get_settings()
@@ -25,7 +25,7 @@ world.apply_settings(settings)
 #-------------------------
 #Spawn ego vehicle
 #-------------------------
-#ego_bp = world.get_blueprint_library().find('vehicle.tesla.model3')
+ego_bp = world.get_blueprint_library().find('vehicle.tesla.model3')
 ego_bp.set_attribute('role_name', 'ego')
 print('\nEgo role_name is set')
 ego_color = random.choice(ego_bp.get_attribute('color').recommended_values)
@@ -37,7 +37,7 @@ number_of_spawn_points = len(spawn_points)
 
 if 0 < number_of_spawn_points:
     random.shuffle(spawn_points)
-    ego_transform = spawn_points[0]
+    ego_transform = spawn_points[5]
     ego_vehicle = world.spawn_actor(ego_bp, ego_transform)
     print('\nEgo is spawned')
 else:
